d5af201fd6133a43dd5bb6148a27675a70e85694
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-06-14 12:43:04 +0100

    added desktop component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from "../utils/api";
 import { recieveUsers } from "../actions/users";
 import { recieveTweets } from "../actions/tweets";
 import { setAuthedUser } from "../actions/authedUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, tweets }) => {
       dispatch(recieveUsers(users));
       dispatch(recieveTweets(tweets));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
